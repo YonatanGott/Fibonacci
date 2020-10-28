4d99b03d8929249bee0020cb8e0d4e495a9a9683
YonatanGott <godiner@gmail.com>
2020-10-28 11:02:09 +0200

    update milestone 4

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-document.getElementById("btn").addEventListener("click", fetch());
 
-const fibserver =  'http://localhost:5050/fibonacci/7';
+function fibFetch(){
+  const fibserver =  'http://localhost:5050/fibonacci/';
+  let number = document.getElementById("number").value;
+  let numberserver = fibserver+number;
+  fetch (numberserver)   
+  .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let fibjson = JSON.parse(data);
+      let result = document.getElementById("result");
+      result.innerHTML = fibjson.result;
+    });
+}
 
-fetch (fibserver)   
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    let result = document.getElementById("result");
-    result.innerHTML = data.result;
-  });
+document.getElementById("btn").addEventListener('click', fibFetch);
+
