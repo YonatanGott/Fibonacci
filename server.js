43b3d710232f9772a759b180091cf28a70d55fe3
YonatanGott <godiner@gmail.com>
2020-10-29 09:06:56 +0200

    validation update

--- server.js
+++ server.js
@@ -12,18 +12,27 @@ function showSpinner() {
 function fibFetch() {
   const fibserver = 'http://localhost:5050/fibonacci/';
   let number = document.getElementById("number").value;
-  let numberserver = fibserver + number;
-  showSpinner()
-  fetch(numberserver)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("result").innerHTML =  data.result; 
-      console.log(data);
-    })
-    .catch(error => {
-      let boop = JSON.stringify(response);
-      console.log(boop);
-      console.log(error);
-      console.log(event);
-    });
+  let fifty = document.getElementById("feedback");
+  if (number > 50) {
+    fifty.classList.add('show2');
+    document.getElementById("result").innerHTML = "";
+    return;
+  }
+  else {
+    fifty.classList.remove('show2');
+    let numberserver = fibserver + number;
+    showSpinner()
+    fetch(numberserver)
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById("result").innerHTML = data.result;
+        console.log(data);
+      })
+      .catch(error => {
+        let boop = JSON.stringify(response);
+        console.log(boop);
+        console.log(error);
+        console.log(event);
+      });
+  }
 }
